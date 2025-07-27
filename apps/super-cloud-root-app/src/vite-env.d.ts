d258a0c1b95b7aa8ffda364e92cf1fd4c658bcc1
Supremacy <3118654731@qq.com>
2025-07-27 13:31:19 +0800

    feat: 新增环境变量声明文件

--- apps/super-cloud-root-app/src/vite-env.d.ts
+++ apps/super-cloud-root-app/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly MYAPP_LOGIN_BASE_URL: string
+    readonly MYAPP_MAIL_USER: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
